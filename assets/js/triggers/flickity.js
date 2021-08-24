23c7135abdc725a6329e7b088a09e13154924cc6
Michał Białkowski <michal@bialkowski.net.pl>
2021-08-25 00:09:50 +0200

    Initial commit

---
+++ assets/js/triggers/flickity.js
@@ -0,0 +1,11 @@
+$(window).on('load', function() {
+	$('.flickity--slider').flickity({
+		cellAlign: 'left',
+		contain: true,
+		freeScroll: false,
+		wrapAround: true,
+		pageDots: true,
+		autoPlay: false,
+		prevNextButtons: true
+	});
+});
