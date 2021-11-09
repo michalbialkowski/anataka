4162a077719251f3c4a4d719347552c114e10b0f
Michał Białkowski <michal@bialkowski.net.pl>
2021-11-09 22:39:09 +0100

    Update

--- assets/js/triggers/flickity.js
+++ assets/js/triggers/flickity.js
@@ -8,13 +8,15 @@ $(window).on('load', function() {
 		autoPlay: false,
 		prevNextButtons: false
 	});
+	
+	$('#tabs__promo .nav a[data-toggle="pill"]').on('shown.bs.tab', function(event) {
+		let elem_id = $(this).attr('href');
+
+		$(elem_id + ' .flickity--slider').flickity('resize');
+
+		//alert(elem_id);
+	});
 });
 
-$('#tabs__promo .nav a[data-toggle="pill"]').on('shown.bs.tab', function(event) {
-	let elem_id = $(this).attr('href');
-	
-	$(elem_id + ' .flickity--slider').flickity('resize');
-	
-	//alert(elem_id);
-});
+
 
